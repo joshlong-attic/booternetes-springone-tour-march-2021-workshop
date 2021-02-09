51bbd1f32dec5feec590d1b70bd1937f99a41365
Josh Long <josh@joshlong.com>
2021-02-08 23:12:20 -0800

    up

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# booternetes-springonetour-2021
-I do not like work even when someone else is doing it.  ― Mark Twain
+# Booternetes (SpringOne Tour 2021)
+
+
+
+
+> I do not like work even when someone else is doing it.  ―Mark Twain
+
+
+We're with Mr. Twain on this one. we loathe work, especially undue work, but getting to production today means doing more of it than ever. 
+
+
+
+* reactive orders, customers 
+* reactive gateway
+* orders, customers, gateway will all use the Wavefront starter (Cora?)
+
+
+
+* 
---
+++ code/customers/src/main/java/com/example/customers/CustomersApplication.java
@@ -0,0 +1,74 @@
+package com.example.customers;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.r2dbc.core.DatabaseClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class CustomersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomersApplication.class, args);
+    }
+
+    @Bean
+    ApplicationListener<ApplicationReadyEvent> ready(
+            DatabaseClient dbc,
+            CustomerRepository customerRepository) {
+        return readyEvent -> {
+
+            var ddl = dbc.sql(
+                    "create table customer( id serial primary key, name varchar(255) not null )")
+                    .fetch()
+                    .rowsUpdated();
+            var names = Flux
+                    .just("Cora", "Coté", "Jakub", "Josh", "Mario", "Mark", "Nate", "Paul", "Tasha", "Tiffany")
+                    .map(name -> new Customer(null, name))
+                    .flatMap(customerRepository::save);
+
+            ddl.thenMany(names).thenMany(customerRepository.findAll()).subscribe(System.out::println);
+
+        };
+    }
+
+}
+
+@RestController
+@RequiredArgsConstructor
+class CustomerRestController {
+
+    private final CustomerRepository customerRepository;
+
+    @GetMapping("/customers")
+    Flux<Customer> get() {
+        return this.customerRepository.findAll();
+    }
+}
+
+
+interface CustomerRepository extends ReactiveCrudRepository<Customer, Integer> {
+
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Customer {
+
+    @Id
+    private Integer id;
+    private String name;
+
+}
