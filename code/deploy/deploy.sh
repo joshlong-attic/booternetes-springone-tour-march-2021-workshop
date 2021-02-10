e173774e30d1809652f368bbb61d445c9519f815
Josh Long <josh@joshlong.com>
2021-02-10 14:47:45 -0800

    deploy

---
+++ code/deploy/deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+echo "going to install everything into Kubernetes"
+
+
+## Orders
+
+function deploy(){
+  module=$1
+#  image_tag="${bp_mode_lowercase}${github_sha:-}"
+  gcr_image_name=gcr.io/${PROJECT_ID}/${module}
+  image_name=${gcr_image_name}:${image_tag}
+  echo "the module is " $module
+
+  cd $module
+  ls -la
+  mvn spring-boot:build-image -Dimage.name=
+}
