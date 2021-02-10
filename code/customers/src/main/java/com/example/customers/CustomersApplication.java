69c3ac196efde3b0bed2ae002c27e46059c798be
Josh Long <josh@joshlong.com>
2021-02-09 21:36:08 -0800

    up

--- code/customers/src/main/java/com/example/customers/CustomersApplication.java
+++ code/customers/src/main/java/com/example/customers/CustomersApplication.java
@@ -1,20 +1,7 @@
 package com.example.customers;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.springframework.r2dbc.core.DatabaseClient;
-import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class CustomersApplication {
