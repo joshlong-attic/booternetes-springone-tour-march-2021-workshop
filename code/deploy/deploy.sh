11440a3802638cd4747f9ee4becd6e62b1b1cfea
Josh Long <josh@joshlong.com>
2021-02-11 01:46:46 -0800

    deploy

--- code/deploy/deploy.sh
+++ code/deploy/deploy.sh
@@ -8,26 +8,71 @@ echo "going to install everything into Kubernetes"
 ## TODO remove
 
 export PROJECT_ID=bootiful
+export START_DIR=$(cd $(dirname $0) && pwd)
+export IMAGE_TAG="${GITHUB_SHA:-$(git rev-parse HEAD)}"
+export ORDER_PORT=8083
+export CUSTOMER_PORT=8084
+export GATEWAY_PORT=8087
+
+echo $IMAGE_TAG
 
 function deploy() {
   MODULE=$1
-  cd $(dirname $0)/../$MODULE
+  cd $START_DIR/../$MODULE
+  pwd
   ROOT_DIR=$(pwd)
   APP_NAME=$MODULE
   TAG_NAME=${1:-$(date +%s)}
-  IMAGE_TAG="${GITHUB_SHA:-$(git rev-parse HEAD)}"
-  echo $IMAGE_TAG
   GCR_IMAGE_NAME=gcr.io/${PROJECT_ID}/${APP_NAME}
-  mvn -f pom.xml \
-    clean spring-boot:build-image \
-    -e -Dspring.profiles.active=production,cloud \
-    -Dspring-boot.build-image.imageName=$GCR_IMAGE_NAME
-  IMAGE_ID=$(docker images -q $GCR_IMAGE_NAME)
-  docker tag "${IMAGE_ID}" ${GCR_IMAGE_NAME}:latest
-  docker tag "${IMAGE_ID}" ${GCR_IMAGE_NAME}:${IMAGE_TAG}
-  docker push ${GCR_IMAGE_NAME}:latest
-  docker push ${GCR_IMAGE_NAME}:${IMAGE_TAG}
 
+  docker images -q $GCR_IMAGE_NAME | while read l; do
+    docker rmi -f $l
+  done
+
+  mvn -f pom.xml \
+    -DskipTests=true \
+    clean spring-boot:build-image -e \
+    -Dspring.profiles.active=production,cloud \
+    -Dspring-boot.build-image.imageName=$GCR_IMAGE_NAME
+
+  IMAGE_ID="$(docker images -q $GCR_IMAGE_NAME)"
+  echo foudn the image id $IMAGE_ID
+  docker tag "${IMAGE_ID}" ${GCR_IMAGE_NAME}:latest
+  docker push ${GCR_IMAGE_NAME}:latest
 }
 
-deploy customers
+#deploy orders
+#deploy customers
+#deploy gateway
+
+## Deploy the Orders Module
+MODULE=orders
+PORT=$ORDER_PORT
+docker run \
+  --env SPRING_RSOCKET_SERVER_PORT=$PORT \
+  -p $PORT:$PORT \
+  --expose $PORT \
+  --rm -d  \
+  gcr.io/bootiful/$MODULE
+
+## Deploy the Customers Module
+MODULE=customers
+PORT=$CUSTOMER_PORT
+docker run \
+  --env SERVER_PORT=$PORT \
+  -p $PORT:$PORT \
+  --expose $PORT \
+  --rm -d  \
+  gcr.io/bootiful/$MODULE
+
+## Deploy the Gateway Module
+MODULE=gateway
+PORT=$GATEWAY_PORT
+docker run \
+  --env SERVER_PORT=$PORT \
+  --env GATEWAY_CUSTOMERS_HOSTNAME_AND_PORT=http://host.docker.internal:$CUSTOMER_PORT \
+  --env GATEWAY_ORDERS_HOSTNAME_AND_PORT=tcp://host.docker.internal:$ORDER_PORT \
+  -p $PORT:$PORT \
+  --expose $PORT \
+  --rm \
+  gcr.io/bootiful/$MODULE
--- code/gateway/src/main/java/com/example/gateway/ApiAdapterConfiguration.java
+++ code/gateway/src/main/java/com/example/gateway/ApiAdapterConfiguration.java
@@ -1,5 +1,6 @@
 package com.example.gateway;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,8 +9,17 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 import java.net.URI;
 
+@Log4j2
 @Configuration
 class ApiAdapterConfiguration {
+
+	ApiAdapterConfiguration(
+		@Value("${gateway.customers.hostname-and-port}") String customersHostAndPort,
+		@Value("${gateway.orders.hostname-and-port}") String ordersHostAndPort
+	) {
+		log.info( "customers endpoint: " + customersHostAndPort);
+		log.info( "orders endpoint: " + ordersHostAndPort);
+	}
 
 	@Bean
 	WebClient webClient(WebClient.Builder builder) {
